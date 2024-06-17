f62ff856206fe993a02b2e0893ad8d3227e1dd9c
DevKhareRBG <dev@rbg.ai>
2024-06-17 14:31:06 +0400

    update

--- src/main/java/com/eazybytes/accounts/exception/CustomerAlreadyExistsException.java
+++ src/main/java/com/eazybytes/accounts/exception/CustomerAlreadyExistsException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class CutomerAlreadyExistsException extends RuntimeException {
+public class CustomerAlreadyExistsException extends RuntimeException {
 
     public CustomerAlreadyExistsException(String message){
         super(message);
