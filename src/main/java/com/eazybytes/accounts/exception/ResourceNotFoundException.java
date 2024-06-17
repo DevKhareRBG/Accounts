896c358231d2f34a1f58e9515f026627c2efc9c9
DevKhareRBG <dev@rbg.ai>
2024-06-17 15:19:34 +0400

    update

--- src/main/java/com/eazybytes/accounts/exception/ResourceNotFoundException.java
+++ src/main/java/com/eazybytes/accounts/exception/ResourceNotFoundException.java
@@ -3,10 +3,10 @@ package com.eazybytes.accounts.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
-    public ResourceNotFoundException(String message){
-        super(message);
+    public ResourceNotFoundException(String resourceName, String fieldName, String fieldValue){
+        super(String.format("%s not found with the given input data%s", resourceName, fieldName, fieldValue));
     }
 }
